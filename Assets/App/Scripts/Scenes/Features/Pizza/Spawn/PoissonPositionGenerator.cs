833c9e014c2c6a28c4765d975efbbe400d2a8fd5
Maksim Zabelo <maxi2000138@gmail.com>
2025-11-01 19:38:26 +0300

    added position generator interface

---
+++ Assets/App/Scripts/Scenes/Features/Pizza/Spawn/PoissonPositionGenerator.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace App.Scripts.Scenes.Features.PizzaData.Spawn
+{
+  public class PoissonPositionGenerator
+  {
+    public List<Vector3> GeneratePositions(float radius, int count)
+    {
+      
+    }
+  }
+}
