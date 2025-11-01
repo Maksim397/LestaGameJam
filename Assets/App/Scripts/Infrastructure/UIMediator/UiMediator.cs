dcb94a22df6e878beab22ab85ae7ed8a92256a04
Ilya Kochev <kochev0987@gmail.com>
2025-11-01 19:18:44 +0300

    Added Timer

--- Assets/App/Scripts/Infrastructure/UIMediator/UiMediator.cs
+++ Assets/App/Scripts/Infrastructure/UIMediator/UiMediator.cs
@@ -1,32 +1,45 @@
-﻿using App.Scripts.Libs.LoadingScreen;
+﻿using System;
+using App.Scripts.Libs.LoadingScreen;
 using UnityEngine;
 
 namespace App.Scripts.Infrastructure.UIMediator
 {
-  public class UiMediator : MonoBehaviour
-  {
-    [SerializeField] private LoadingCurtain _loadingScreen;
-    [SerializeField] private PauseWindow _pauseWindow;
-    [SerializeField] private WinWindow _winWindow;
-    [SerializeField] private GameOverWindow _gameOverWindow;
-    [SerializeField] private StartWindow _startWindow;
-    [SerializeField] private InGameWindow _inGameWindow;
-    public void ShowLoadingScreen() => _loadingScreen.Show();
-    public void HideLoadingScreen() => _loadingScreen.Hide();
-    
-    public void ShowPauseWindow() => _pauseWindow.Show(); 
-    public void HidePauseWindow() => _pauseWindow.Hide();
+    public class UiMediator : MonoBehaviour
+    {
+        [SerializeField] private LoadingCurtain _loadingScreen;
+        [SerializeField] private PauseWindow _pauseWindow;
+        [SerializeField] private WinWindow _winWindow;
+        [SerializeField] private GameOverWindow _gameOverWindow;
+        [SerializeField] private StartWindow _startWindow;
+        [SerializeField] private InGameWindow _inGameWindow;
 
-    public void ShowWinWindow() => _winWindow.Show();
-    public void HideWinWindow() => _winWindow.Hide();
+        private bool _isTimerRunning;
 
-    public void ShowGameOverWindow() => _gameOverWindow.Show();
-    public void HideGameOverWindow() => _gameOverWindow.Hide();
-    
-    public void ShowStartWindow() => _startWindow.Show();
-    public void HideStartWindow() => _startWindow.Hide();
+        public void ShowLoadingScreen() => _loadingScreen.Show();
+        public void HideLoadingScreen() => _loadingScreen.Hide();
 
-    public void ShowInGameWindow() => _inGameWindow.Show();
-    public void HideInGameWindow() => _inGameWindow.Hide();
-  }
+        public void ShowPauseWindow() => _pauseWindow.Show();
+        public void HidePauseWindow() => _pauseWindow.Hide();
+
+        public void ShowWinWindow() => _winWindow.Show();
+        public void HideWinWindow() => _winWindow.Hide();
+
+        public void ShowGameOverWindow() => _gameOverWindow.Show();
+        public void HideGameOverWindow() => _gameOverWindow.Hide();
+
+        public void ShowStartWindow() => _startWindow.Show();
+        public void HideStartWindow() => _startWindow.Hide();
+
+        public void ShowInGameWindow() => _inGameWindow.Show();
+        public void HideInGameWindow() => _inGameWindow.Hide();
+
+        public void Start()
+        {
+            StartCountdown(TimeSpan.FromSeconds(60f));
+        }
+
+        public void StartCountdown(TimeSpan startTime) => _inGameWindow.StartTimer(startTime);
+
+        public void StopCountdown() => _inGameWindow.StopTimer();
+    }
 }
