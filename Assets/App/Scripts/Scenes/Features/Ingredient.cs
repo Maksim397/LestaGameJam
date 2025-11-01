d0057888597f047a5175c10501559d68ac93df27
Maksim Zabelo <maxi2000138@gmail.com>
2025-11-01 16:47:01 +0300

    added ingredient base class

---
+++ Assets/App/Scripts/Scenes/Features/Ingredient.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Ingredient : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+  public void OnBeginDrag(PointerEventData eventData)
+  {
+    
+  }
+  
+  public void OnDrag(PointerEventData eventData)
+  {
+    
+  }
+  
+  public void OnEndDrag(PointerEventData eventData)
+  {
+    
+  }
+}
