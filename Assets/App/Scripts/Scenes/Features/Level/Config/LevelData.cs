b290d36c53af061fe306986a0927610ce4cd04ca
Ilya Kochev <kochev0987@gmail.com>
2025-11-02 15:06:16 +0300

    LevelConfig add timer

--- Assets/App/Scripts/Scenes/Features/Level/Config/LevelData.cs
+++ Assets/App/Scripts/Scenes/Features/Level/Config/LevelData.cs
@@ -1,9 +1,12 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using Sirenix.Serialization;
 
 namespace App.Scripts.Scenes.Features.Level
 {
   public class LevelData
   {
-    public List<PizzaVariants> Pizzas; 
+    public List<PizzaVariants> Pizzas;
+    public float levelTimeSeconds;
   }
 }
